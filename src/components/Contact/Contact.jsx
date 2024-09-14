b7e1bd28c7b223a3267c771583fb2338cc7150de
Stuti Gupta <juhiexcusestuti@gmail.com>
2024-09-14 14:49:53 +0530

    Create a Portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:stuticoder123@gmail.com">stuticoder123@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/stuti-gupta-0374952a9">linkedin.com/in/stuti-gupta-0374952a9</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/Stuticoder1">github.com/Stuticoder1</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
